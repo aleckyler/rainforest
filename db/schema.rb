d5e4964dad4d1dcb52f69a7982afbfd3639c3cf5
Alec Greenberg <aleckyler@comcast.com>
2015-07-29 16:38:16 -0400

    add scaffold for pictures

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150729185832) do
+ActiveRecord::Schema.define(version: 20150729203737) do
+
+  create_table "pictures", force: :cascade do |t|
+    t.string   "url"
+    t.integer  "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "pictures", ["product_id"], name: "index_pictures_on_product_id"
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
